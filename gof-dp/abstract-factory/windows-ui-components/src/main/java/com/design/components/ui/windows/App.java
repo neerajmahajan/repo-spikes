11327fd3ddfd8a2893f4a7f5b2d58f380f5326b9
neerajmahajan <neerajmahajan82@gmail.com>
2015-06-09 15:53:46 +0100

    Added platform specific components

---
+++ gof-dp/abstract-factory/windows-ui-components/src/main/java/com/design/components/ui/windows/App.java
@@ -0,0 +1,13 @@
+package com.design.components.ui.windows;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
