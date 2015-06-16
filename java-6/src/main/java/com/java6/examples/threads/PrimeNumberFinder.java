d495d62769b3b220ea58d0df258f63725a2a3bee
vishalm <sunnymahajan89@gmail.com>
2015-06-16 19:37:45 +0530

    Done changes in Prime Number Finder

--- java-6/src/main/java/com/java6/examples/threads/PrimeNumberFinder.java
+++ java-6/src/main/java/com/java6/examples/threads/PrimeNumberFinder.java
@@ -7,31 +7,31 @@ public class PrimeNumberFinder implements Runnable {
 
 	private long start;
 	private long stop;
-	
-	public PrimeNumberFinder(long start, long stop){
+
+	public PrimeNumberFinder(long start, long stop) {
 		this.start = start;
 		this.stop = stop;
 	}
 
 	public void run() {
-		
-		List<Long> primeNumberList =  new ArrayList<Long>();
-		
-		for(long i=start;i<= stop; start ++){
-			if(isPrime(i));{
+		List<Long> primeNumberList = new ArrayList<Long>();
+
+		for (long i = start; i <= stop; i++) {
+			if (isPrime(i)) {
 				primeNumberList.add(i);
 			}
 		}
+		System.out.println(primeNumberList);
 	}
 
 	protected boolean isPrime(long number) {
 		boolean primeNumber = true;
 
-		if (number == 2) {
-			return primeNumber;
+		if (number == 2 || number == 1) {
+			return false;
 		}
-		
-		for (int divisor = 3; divisor < number; divisor++)
+
+		for (int divisor = 2; divisor < number / 2; divisor++)
 			if (number % divisor == 0) {
 				return false;
 			}
--- java-6/src/main/java/com/java6/examples/threads/TestPrimeNumberFinder.java
+++ java-6/src/main/java/com/java6/examples/threads/TestPrimeNumberFinder.java
@@ -1,12 +1,27 @@
 package com.java6.examples.threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TestPrimeNumberFinder {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		PrimeNumberFinder primeNumberFinder = new PrimeNumberFinder(1, 20000);
+		PrimeNumberFinder primeNumberFinder2 = new PrimeNumberFinder(20001, 40000);
+		PrimeNumberFinder primeNumberFinder3 = new PrimeNumberFinder(40001, 60000);
+		PrimeNumberFinder primeNumberFinder4 = new PrimeNumberFinder(60001, 80000);
+		PrimeNumberFinder primeNumberFinder5 = new PrimeNumberFinder(80001, 100000);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(5);			// create 5 threads...
+		executorService.execute(primeNumberFinder);
+		executorService.execute(primeNumberFinder2);
+		executorService.execute(primeNumberFinder3);
+		executorService.execute(primeNumberFinder4);
+		executorService.execute(primeNumberFinder5);
+
+		executorService.shutdown();
+
 	}
 
 }
