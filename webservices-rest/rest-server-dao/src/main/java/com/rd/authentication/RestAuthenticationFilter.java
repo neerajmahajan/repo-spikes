3c52f48a259e4469f3a4d80f8f6bade2bfabee8a
vishalmahajan12 <sunnymahajan89@gmail.com>
2015-08-25 01:21:44 +0530

    change constant visibility

--- webservices-rest/rest-server-dao/src/main/java/com/rd/authentication/RestAuthenticationFilter.java
+++ webservices-rest/rest-server-dao/src/main/java/com/rd/authentication/RestAuthenticationFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.rd.authentication.AuthenticationService;
 
 public class RestAuthenticationFilter implements Filter {
-	private static final String UNAME = "uname";
+	public static final String UNAME = "uname";
 	public static final String AUTHENTICATION_HEADER = "Authorization";
 
 	public void doFilter(ServletRequest request, ServletResponse response,
